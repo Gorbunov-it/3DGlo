ff95817b615a62ec2a1e7e8c271801f5ce8565bd
Ivan Gorbunov <I.MGorbunov@yandex.ru>
2023-12-23 16:31:12 +0400

    Исправление Lesson19

--- dist/date/main.js
+++ dist/date/main.js
@@ -4,6 +4,29 @@ const date = new Date();
 const newYaerDay = new Date("31 december 2023");
 const week = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
 
+const timesOfDay = () => {
+  let getHours = "";
+  const hour = date.getHours();
+  switch (true) {
+    case hour > 0 && hour <= 6:
+      getHours = "Доброй ночи";
+      break;
+    case hour > 6 && hour <= 12:
+      getHours = "Доброе утро";
+      break;
+    case hour > 12 && hour <= 18:
+      getHours = "Добрый день";
+      break;
+    case hour > 18 && hour <= 24:
+      getHours = "Добрый вечер";
+      break;
+    default:
+      getHours = "Невозможно определить тип суток";
+      break;
+  }
+  return getHours;
+};
+
 const getCurrentDay = function () {
   const day = date.toLocaleString();
   const myDate = new Date(day.replace(/(\d+).(\d+).(\d+)/, "$3/$2/$1"));
@@ -36,6 +59,7 @@ const beforeTimeNewYear = () => {
 };
 
 const start = () => {
+  console.log(`${timesOfDay()}`);
   console.log(`Сегодня: ${getCurrentDay()}`);
   console.log(`Текущее время: ${CurrentTime()}`);
   console.log(`До нового года осталось ${beforeTimeNewYear()}`);
